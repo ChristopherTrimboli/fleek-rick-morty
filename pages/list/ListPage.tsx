ee89964f14a8fbc8dac18d20e0263b9d75533575
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-04-14 12:49:37 -0700

    add left filter bar to list, adjustable columns

--- pages/list/ListPage.tsx
+++ pages/list/ListPage.tsx
@@ -3,8 +3,29 @@ import styled from "styled-components";
 import { useGetCharactersQuery } from "../../api/rickMorty";
 import CharacterCard from "../../components/CharacterCard";
 
-const ListContainer = styled.div`
+const Grid = styled.div`
     height: calc(100% - 121px); // minus nav and padding heights
+    display: flex;
+    flex-direction: row;
+`;
+
+const FiltersContainer = styled.div`
+    height: 100%;
+    min-width: 200px;
+    width: 350px;
+    max-width: 700px;
+    resize: horizontal;
+    border-right: 1px solid grey;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    font-family: Roboto-Mono, Open Sans;
+`;
+
+const ListContainer = styled.div`
+    height: 100%;
     width: 100%;
     overflow-x: auto;
     display: flex;
@@ -29,20 +50,25 @@ const ListPage = memo(() => {
     }, [charactersIsSuccess])
 
     return (
-        <ListContainer>
-            {
-                charactersData?.results?.map((character, index) =>
-                    <CharacterCard
-                        key={index}
-                        characterId={character.id}
-                        imageUrl={character.image}
-                        name={character.name}
-                        species={character.species}
-                        status={character.status}
-                    />
-                )
-            }
-        </ListContainer>
+        <Grid>
+            <FiltersContainer>
+
+            </FiltersContainer>
+            <ListContainer>
+                {
+                    charactersData?.results?.map((character, index) =>
+                        <CharacterCard
+                            key={index}
+                            characterId={character.id}
+                            imageUrl={character.image}
+                            name={character.name}
+                            species={character.species}
+                            status={character.status}
+                        />
+                    )
+                }
+            </ListContainer>
+        </Grid>
     );
 });
 
